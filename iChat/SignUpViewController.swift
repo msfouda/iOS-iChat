cd70ec2b1b4fcec5c79f5c28025384b6d57f285a
mfouda214 <m.fouda@robone.net>
2018-06-26 20:07:10 +0100

    Handle user SignUp

--- iChat/SignUpViewController.swift
+++ iChat/SignUpViewController.swift
@@ -15,7 +15,35 @@ class SignUpViewController: UIViewController {
     @IBOutlet weak var passwordField: UITextField!
     
     @IBAction func didTapSignUp(sender: UIButton) {
+        guard let name = nameField.text,
+            let email = emailField.text,
+            let password = passwordField.text,
+            name.count > 0,
+            email.count > 0,
+            password.count > 0
+            else {
+                self.showAlert(message: "Enter a name, an email and a password.")
+                return
+        }
         
+        Auth.auth().createUser(withEmail: email, password: password) { (user, error) in
+            if let error = error {
+                if error._code == AuthErrorCode.invalidEmail.rawValue {
+                    self.showAlert(message: "Enter a valid email.")
+                } else if error._code == AuthErrorCode.emailAlreadyInUse.rawValue {
+                    self.showAlert(message: "Email already in use.")
+                } else {
+                    self.showAlert(message: "Error: \(error.localizedDescription)")
+                }
+                print(error.localizedDescription)
+                return
+            }
+            
+            if let user = user {
+                self.setUserName(user: user, name: name)
+            }
+        }
+    }
     }
 
     override func viewDidLoad() {
