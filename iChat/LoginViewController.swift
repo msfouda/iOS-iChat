64aa6c18de7aa101dea54955ba2a15c638e69140
mfouda214 <m.fouda@robone.net>
2018-06-26 20:29:37 +0100

    Handle user login

--- iChat/LoginViewController.swift
+++ iChat/LoginViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 class LoginViewController: UIViewController {
 
@@ -14,11 +15,41 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var passwordField: UITextField!
     
     @IBAction func didTapLogin(sender: UIButton) {
+        guard let email = emailField.text, let password = passwordField.text, email.characters.count > 0, password.characters.count > 0 else {
+            self.showAlert(message: "Enter an email and a password.")
+            return
+        }
         
+        Auth.auth().signIn(withEmail: email, password: password) { (user, error) in
+            if let error = error {
+                if error._code == AuthErrorCode.userNotFound.rawValue {
+                    self.showAlert(message: "There are no users with the specified account.")
+                } else if error._code == AuthErrorCode.wrongPassword.rawValue {
+                    self.showAlert(message: "Incorrect username or password.")
+                } else {
+                    self.showAlert(message: "Error: \(error.localizedDescription)")
+                }
+                print(error.localizedDescription)
+                return
+            }
+            
+            if let user = user {
+                AuthenticationManager.sharedInstance.didLogIn(user: user.user)
+                self.performSegue(withIdentifier: "ShowChatsFromLogin", sender: nil)
+            }
+        }
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    // MARK: -
+    // MARK: - Show Alert
+    func showAlert(message: String) {
+        let alertController = UIAlertController(title: "iChat", message: message, preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+        present(alertController, animated: true, completion: nil)
     }
 
 }
