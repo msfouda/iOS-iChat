8431cb2d9c702cc7aa60317cdc00645df4c31e5d
mfouda214 <m.fouda@robone.net>
2018-06-26 20:03:34 +0100

    Cnfigure app with Firebase

---
+++ iChat/AuthenticationManager.swift
@@ -0,0 +1,9 @@
+//
+//  AuthenticationManager.swift
+//  iChat
+//
+//  Created by Mohamed Sobhi  Fouda on 6/26/18.
+//  Copyright © 2018 CareerFoundry. All rights reserved.
+//
+
+import Foundation
